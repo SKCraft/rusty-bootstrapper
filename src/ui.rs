d9c17cfe75d3d754ad97f4f88f3950dab64f02bc
Henry Le Grys <henry@legrys.info>
2020-12-27 00:27:27 +0000

    Use modal dialog function for popups

--- src/ui.rs
+++ src/ui.rs
@@ -6,27 +6,10 @@ pub fn show_dialog(message: &str) {
 
     let ui_msg = message.split(": ").collect::<Vec<&str>>().join("\n");
 
-    let ui = UI::init().unwrap();
-    let mut win = Window::new(&ui, "Launcher Bootstrapper",
+    let ui = UI::init().expect("Failed to initialize UI.");
+    let mut win = Window::new(&ui, "Bootstrap",
                               200, 100, WindowType::NoMenubar);
 
-    let mut vbox = VerticalBox::new(&ui);
-    vbox.set_padded(&ui, true);
-
-    let label = Label::new(&ui, &ui_msg);
-    let mut button = Button::new(&ui, "OK");
-    button.on_clicked(&ui, {
-        let ui = ui.clone();
-        move |_| {
-            ui.quit();
-        }
-    });
-
-    vbox.append(&ui, label, LayoutStrategy::Stretchy);
-    vbox.append(&ui, button, LayoutStrategy::Compact);
-
-    win.set_child(&ui, vbox);
-    win.show(&ui);
-
-    ui.main();
+    win.modal_err(&ui, "Launcher Bootstrapper", &ui_msg);
+    ui.quit();
 }
